d4dea2cb927354f3070cbbd3f8d894fb1b9190ba
Hardik-24 <73469095+Hardik-24@users.noreply.github.com>
2023-08-12 17:40:29 +0530

    a

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,6 @@
+import 'package:bubble_tea/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,18 +12,19 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          children: [
-            Text(
-              'Bubble Tea Shop',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Consumer<BubbleTeaShop>(
+        builder: (context, value, child) => SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Bubble Tea Shop',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 }
